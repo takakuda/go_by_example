f73d5bf3d2dd88bcad8a549760f7ccfe43579c4a
kei <kei.takakuda.0415@icloud.com>
2020-07-26 23:04:11 +0900

    collection-function Include

--- collection-functions.go
+++ collection-functions.go
@@ -13,7 +13,13 @@ func Index(vs []string, t string) int {
 	return -1
 }
 
+func Include(vs []string, t string) bool {
+	return Index(vs, t) >= 0
+}
+
 func main() {
 	strs := []string{"peach", "apple", "pear", "plum"}
 	fmt.Println(Index(strs, "pear"))
+
+	fmt.Println(Include(strs, "grape"))
 }
