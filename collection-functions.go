eae6ee53df38eea50aed243e267754d28920ae1b
takakuda <kei.takakuda.0415@icloud.com>
2020-08-01 22:16:13 +0900

    Any

--- collection-functions.go
+++ collection-functions.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func Index(vs []string, t string) int {
@@ -17,9 +18,22 @@ func Include(vs []string, t string) bool {
 	return Index(vs, t) >= 0
 }
 
+func Any(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	strs := []string{"peach", "apple", "pear", "plum"}
 	fmt.Println(Index(strs, "pear"))
 
 	fmt.Println(Include(strs, "grape"))
+
+	fmt.Println(Any(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
 }
