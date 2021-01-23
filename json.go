445053affe3d4033137c1b8dcddbc08771e7f0b0
kei <kei.takakuda.0415@icloud.com>
2021-01-23 23:13:25 +0900

    json.Marshal slice && map

--- json.go
+++ json.go
@@ -18,4 +18,12 @@ func main() {
 
 	strB, _ := json.Marshal("gopher")
 	fmt.Println(string(strB))
+
+	slcD := []string{"apple", "peach", "pear"}
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println(string(slcB))
+
+	mapD := map[string]int{"apple": 5, "lettuce": 7}
+	mapB, _ := json.Marshal(mapD)
+	fmt.Println(string(mapB))
 }
